9a9cf19ed53acd5bc56acad475b338cd6686f2ec
Franck <franck.spadotto@yahoo.com>
2025-09-02 14:22:00 +0200

    Advanced tasks

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    argc = len(argv) - 1
+
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
+        exit(1)
+
+    a = int(argv[1])
+    ope = argv[2]
+    b = int(argv[3])
+
+    if ope == '+':
+        result = add(a, b)
+    elif ope == '-':
+        result = sub(a, b)
+    elif ope == '*':
+        result = mul(a, b)
+    elif ope == '/':
+        result = float(div(a, b))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, ope, b, result))
+
+
+if __name__ == "__main__":
+    main()

5d6add254b065ff679cfeb5df2409308409d6f3b
Franck <franck.spadotto@yahoo.com>
2025-09-08 15:41:49 +0200

    Don'y use .copy(), it makes a shallow copy

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    new_matrix = matrix.copy()
-
-    return list(map(lambda y: list(map(lambda x: x * x, y)), new_matrix))
+    return list(map(lambda y: list(map(lambda x: x * x, y)), matrix))

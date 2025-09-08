c0d1e36d692c1bf61090a360439aa82678c6e1d3
Franck <franck.spadotto@yahoo.com>
2025-09-08 16:41:03 +0200

    Safe print a list of integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(i), end="")
+            count += 1
+        except TypeError:
+            pass
+    print("")
+    return count
+

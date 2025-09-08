1ae9e5738fcf55bbf98d4abeba0840d65a879652
Franck <franck.spadotto@yahoo.com>
2025-09-08 17:31:56 +0200

    Move count += 1 in 'else'

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,10 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(i), end="")
-            count += 1
         except (ValueError, TypeError):
             pass
+        else:
+            count += 1
+
     print("")
     return count

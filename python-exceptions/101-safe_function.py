522c047192c4e3edca55cb606f5051e1dc97acaf
Franck <franck.spadotto@yahoo.com>
2025-09-08 17:30:39 +0200

    Write all exceptions into stderr

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 
 def safe_function(fct, *args):
     try:
-        return fct(args[0], args[1])
-    except ZeroDivisionError:
-        pass
-    except IndexError:
-        pass
-    else:
-        pass
+        return fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None

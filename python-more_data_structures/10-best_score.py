b2ff89be60062cb7ed3fac0ab9f12fa2c40420e6
Franck <franck.spadotto@yahoo.com>
2025-09-08 14:34:14 +0200

    Return best score name

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best = 0
+    name = None
+    for key in a_dictionary.keys():
+        if best < a_dictionary[key]:
+            best = a_dictionary[key]
+            name = key
+    return name

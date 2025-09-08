8227c6d025327fbb5c8a93c864d9bea633c929c5
Franck <franck.spadotto@yahoo.com>
2025-09-08 15:16:08 +0200

    Return score weight

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    weight = 0
+    div = 0
+    for key, value in my_list:
+        weight += key * value
+        div += value
+    return weight / div

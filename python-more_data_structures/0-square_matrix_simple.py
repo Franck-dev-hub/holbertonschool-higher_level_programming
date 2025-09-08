1d7fad06d0e7aea7c56b6f8731c2d9e8c7cca40c
Franck <franck.spadotto@yahoo.com>
2025-09-08 15:33:46 +0200

    Test: return directly the result

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for i in range(len(new_matrix)):
-        new_matrix[i] = list(map(lambda x: x * x, new_matrix[i]))
-    return new_matrix
+    return [[x * x for x in row] for row in matrix]

776984480f0a556806d10860567aee476d7672ef
Franck <franck.spadotto@yahoo.com>
2025-09-08 12:57:56 +0200

    Check if len == 0

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max = 0
-    for i in my_list:
-        if i > max:
-            max = i
-    return max
+    if len(my_list) == 0:
+        return None
+    else:
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+        return max

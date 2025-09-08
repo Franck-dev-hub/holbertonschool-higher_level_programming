a0934c1198241918f44c2254d7eb3ae01af88fb9
Franck <franck.spadotto@yahoo.com>
2025-09-08 15:51:16 +0200

    Back to the first strategy

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
+    new_matrix = matrix.copy()
+    for i in range(len(new_matrix)):
+        new_matrix[i] = list(map(lambda x: x * x, new_matrix[i]))
+    return (new_matrix)

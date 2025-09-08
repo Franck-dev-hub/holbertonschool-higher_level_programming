7fc5a001cf84cce9abb1561505b1abb660e9b9b0
Franck <franck.spadotto@yahoo.com>
2025-09-08 12:47:09 +0200

    Print with .format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(i)
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print("")

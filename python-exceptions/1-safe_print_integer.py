97369265a3a8b18f1a7ea725644ce2c8635b7e24
Franck <franck.spadotto@yahoo.com>
2025-09-08 17:10:19 +0200

    Use :d instead of int()

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        int(value)
-        print("{}".format(value))
+        print("{:d}".format(value))
         return True
     except ValueError:
         print("{} is not an integer".format(value))

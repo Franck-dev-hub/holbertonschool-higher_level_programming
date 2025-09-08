6fc3a8577e5e48cbfb7137c41a7eb0982846253f
Franck <franck.spadotto@yahoo.com>
2025-09-08 16:29:19 +0200

    Safe print an integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        int(value)
+        print("{}".format(value))
+        return True
+    except ValueError:
+        print("{} is not an integer".format(value))
+        return False

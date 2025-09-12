ba9ee35845674e708bb93be224af18922964ef87
Franck <franck.spadotto@yahoo.com>
2025-09-12 15:36:31 +0200

    Indent a text

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""
+5-text_indentation.py
+This module prints a text with 2 new lines after each of these characters: ., ? and :
+Return: Void
+"""
+
+
+def text_indentation(text):
+    """
+    text_indentation - Indent text
+    @text: Text to indent
+    Return: Void
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    temp = ""
+    for char in text:
+        temp += char
+        if char in ".,?:":
+            print(temp.strip(), end="\n\n")
+            temp = ""
+
+    if temp.strip() != "":
+        print(temp.strip(), end="")

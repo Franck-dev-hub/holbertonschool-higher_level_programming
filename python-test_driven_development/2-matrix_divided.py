989d07a85567522d6c5f29c4f221c8f008557423
Franck <franck.spadotto@yahoo.com>
2025-09-12 10:30:46 +0200

    Add comments & backspaces for pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
+"""
+2-matrix_divided.py
+This module divides all elements of a matrix
+Return a matrix
+"""
+
+
 def matrix_divided(matrix, div):
+    """
+    Main divide function
+    """
     errors(matrix, div)
 
     new_matrix = [row.copy() for row in matrix]
@@ -9,7 +19,11 @@ def matrix_divided(matrix, div):
             new_matrix[row][col] = round(num / div, 2)
     return new_matrix
 
+
 def errors(matrix, div):
+    """
+    Error management function
+    """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     elif div == 0:
@@ -18,9 +32,14 @@ def errors(matrix, div):
     last_row = matrix[0]
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                    "matrix must be a matrix"
+                    " (list of lists) of integers/floats"
+                    )
         if len(row) != len(last_row):
             raise TypeError("Each row of the matrix must have the same size")
         for num in row:
             if not isinstance(num, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                        "matrix must be a matrix"
+                        " (list of lists) of integers/floats")

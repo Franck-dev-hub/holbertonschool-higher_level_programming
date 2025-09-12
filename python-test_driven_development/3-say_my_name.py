98225effcd786be7ec8ba90a3befdc76823e8c43
Franck <franck.spadotto@yahoo.com>
2025-09-12 14:02:16 +0200

    Update doctest & comments

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+"""
+3-say_my_name.py
+This module prints the first name and the last name
+Return: Void
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    say_my_name - Print first_name & last_name
+    @first_name: First name like "John"
+    @last_name: Last name like "Doe"
+    Return: Void
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

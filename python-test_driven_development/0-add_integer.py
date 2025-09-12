dd8bb54d6026383e5ac213b9eec22d6b840d59b9
Franck <franck.spadotto@yahoo.com>
2025-09-12 10:17:17 +0200

    Extend comments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,13 +2,15 @@
 
 """
 0-add_integer.py
-Add two numbers
+This module add two integers or floats
 Return int
 """
 
+
 def add_integer(a, b=98):
     """
-    Add two numbers
+    This function add two integers or floats
+    Floats are casted to integers before addition
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")

4da2f0d8a0c1fda1d27ebb4f44e5c0281a6c416a
Franck <franck.spadotto@yahoo.com>
2025-09-12 14:54:53 +0200

    Print a # square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""
+4-print_square.py
+This module prints a square with the char #
+Return Void
+"""
+
+
+def print_square(size):
+    """
+    print_square - It prints a square with char #
+    @size: Size of the square
+    Return: Void
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")

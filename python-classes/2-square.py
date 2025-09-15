fb21cd7406ec82938a6d01900d89e30e7a18ec68
Franck <franck.spadotto@yahoo.com>
2025-09-15 10:51:32 +0200

    Update pycodestyle + add comments

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,15 +6,20 @@ This module create a square
 Return: Void
 """
 
+
 class Square:
     """
     Define a square
     """
     def __init__(self, size=0):
+        """
+        __init__ - Init new square
+        @size: Size of the square
+        Return: Void
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-

679816f955e968aa588bd0c69d842de0cb481f05
Franck <franck.spadotto@yahoo.com>
2025-09-15 11:00:06 +0200

    Calculate square area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+
+"""
+3-square.py
+This module create a square
+Return: Void
+"""
+
+
+class Square:
+    """
+    Define a square
+    """
+    def __init__(self, size=0):
+        """
+        __init__ - Init new square
+        @size: Size of the square
+        Return: Void
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        area - Calculate square area
+        Return: int - area value
+        """
+        return (self._Square__size * self._Square__size)

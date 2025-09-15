61839a1b30a01252bc641d848dece75e7c7a4394
Franck <franck.spadotto@yahoo.com>
2025-09-15 09:48:28 +0200

    Create private attribute 'size'

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""
+1-square.py
+This module create a square
+Return: Void
+"""
+
+class Square:
+    """
+    Define a square
+    """
+    def __init__(self, size):
+        self.__size = size

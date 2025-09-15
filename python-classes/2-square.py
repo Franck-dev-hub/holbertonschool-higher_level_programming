6d92cdb1e9bb7775ff9a0ff052dded564e48b472
Franck <franck.spadotto@yahoo.com>
2025-09-15 10:46:53 +0200

    Init size to 0 + raise errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+"""
+2-square.py
+This module create a square
+Return: Void
+"""
+
+class Square:
+    """
+    Define a square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+

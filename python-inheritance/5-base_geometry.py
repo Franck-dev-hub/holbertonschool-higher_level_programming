1189b1f325e3da04ed3524fe2905bef9cf8e9cd8
Franck <franck.spadotto@yahoo.com>
2025-09-22 11:23:47 +0200

    Create BaseGeomtry class

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""
+5-base_geometry.py
+Class 'BaseGeometry'
+Return Void
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry - A class
+    Return: Void
+    """
+    pass

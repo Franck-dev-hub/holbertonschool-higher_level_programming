b146172be53729405fada01e9697392d79325270
Franck <franck.spadotto@yahoo.com>
2025-09-22 13:46:55 +0200

    Invert == & != returns

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""
+100-my_int.py
+Invert == and != values
+"""
+
+
+class MyInt(int):
+    """
+    MyInt - Custom int
+    """
+    def __eq__(self, value):
+        """
+        __eq__ - Invert == operator
+        @value: Value to be checked
+        Return: self != value
+        """
+        return int(self) != value
+
+    def __ne__(self, value):
+        """
+        __ne__ - Invert != operator
+        @value: Value to be checked
+        Return: self == value
+        """
+        return int(self) == value

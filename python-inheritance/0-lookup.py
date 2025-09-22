754779bf0c124578d14e39a57ca4a42f10cca3ce
Franck <franck.spadotto@yahoo.com>
2025-09-22 09:58:37 +0200

    Return avaible attributes & methods

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""
+0-lookup.py
+Return a list of avaiable attributes ans methods of an object
+Return: A list object
+"""
+
+def lookup(obj):
+    """
+    Look object attributes & methods
+    Return: A list object
+    """
+    return dir(obj)

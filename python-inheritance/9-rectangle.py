fdd23717495e3b5ce40a94cc5e7edd67b85d3548
Franck <franck.spadotto@yahoo.com>
2025-09-22 13:29:25 +0200

    Add area method & str output

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+
+"""
+9-rectangle.py
+Define Rectangle that inherits from BaseGeometry
+Return: Void
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle - Set a rectangle object
+    Return: Void
+    """
+    def __init__(self, width, height):
+        """
+        __init__ - Init function
+        @width: Width value
+        @height: Height value
+        Return: Void
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """
+        __str__ - Human readable output
+        Return: String
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        area - Calculate the area of the recangle
+        Return: int - the area of the rectangle
+        """
+        return (self.__width * self.__height)

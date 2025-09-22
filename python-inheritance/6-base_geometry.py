2d8385f054794bb3b7aaf2720754e918af0d05dd
Franck <franck.spadotto@yahoo.com>
2025-09-22 11:28:00 +0200

    Raise an error

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+"""
+6-base_geometry.py
+Class 'BaseGeometry'
+Return Error
+"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry - A class
+    Return: Void
+    """
+    def area(self):
+        """
+        area - Not implemented
+        Return: Error
+        """
+        raise Exception("area() is not implemented")

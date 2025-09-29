3668968cdf418d17ba515e4b8340007aba919e37
Franck <franck.spadotto@yahoo.com>
2025-09-29 11:19:52 +0200

    print text from a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""
+0-read_file.py
+Read a txt file
+"""
+
+def read_file(filename=""):
+    """
+    read_file - Read a txt file
+    @filename: Name of the file
+    Return: Void
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())

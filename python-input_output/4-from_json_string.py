6ff7c96f935431ec5ef5a85b6b04498ca93ecb7d
Franck <franck.spadotto@yahoo.com>
2025-09-29 13:09:32 +0200

    Create an object from json

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+import json
+
+"""
+4-from_json_string.py
+Return un object represented by a JSON string
+"""
+
+def from_json_string(my_str):
+    """
+    from_json_string - Create an object from a json
+    @my_str: String to parse
+    Return: An object
+    """
+    return json.loads(my_str)

d103923a7eab2c52e31c79b14fc6e067a08a5357
Franck <franck.spadotto@yahoo.com>
2025-09-29 13:42:19 +0200

    Create a dict description from an object

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""
+8-class_to_json.py
+Create a dictionary description from an object
+"""
+
+
+def class_to_json(obj):
+    """
+    class_to_json - Create a dict description from an object
+    @obj: Object to parse
+    Return: Dict object
+    """
+    return obj.__dict__

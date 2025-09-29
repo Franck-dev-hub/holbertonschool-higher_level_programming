d267b3f614a08d68a74fa4159238c1c9588fc107
Franck <franck.spadotto@yahoo.com>
2025-09-29 15:26:46 +0200

    Display Pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""
+12-pascal_triangle.py
+Print the Pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    pascal_triangle - Display Pascal triangle
+    @n: Number of triangle lines
+    Return: List that image a triangle
+    """
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle

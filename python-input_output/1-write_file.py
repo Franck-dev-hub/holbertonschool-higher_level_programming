08ece9f22b682823e75b70a6d4f98fd9f0da33e6
Franck <franck.spadotto@yahoo.com>
2025-09-29 11:32:22 +0200

    Write text and return number of char

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""
+1-write_file.py
+Write in a file
+"""
+
+def write_file(filename="", text=""):
+    """
+    write_file - Write in a file
+    @filename: Name of the file
+    @text: Text to insert
+    Return: Number of char written
+    """
+    counter = 0
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)

a15cd5723985ce8df1b4c9c0c9f753237fa03d03
Franck <franck.spadotto@yahoo.com>
2025-09-29 12:56:50 +0200

    Return a json representation

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+import json
+
+"""
+3-to_json_string.py
+Get json informations
+"""
+
+def to_json_string(my_obj):
+    """
+    to_json_string - Create a json from an object
+    @my_obj: Object to parse
+    Return: Json representation
+    """
+    return json.dumps(my_obj)

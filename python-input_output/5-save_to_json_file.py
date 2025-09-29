f8ced1f657fe8b2a493a1604f85c22874bafb87f
Franck <franck.spadotto@yahoo.com>
2025-09-29 13:14:46 +0200

    Write in a json file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+import json
+
+"""
+5-save_to_json_file.py
+Save an object in a json file
+"""
+
+def save_to_json_file(my_obj, filename):
+    """
+    save_to_json_file - Write in a json file
+    @my_obj: Object to write in json file
+    @filename: Name of json file
+    Return: Void
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        json.dump(my_obj, file)

e92f78c3853be6f129f93d9d3dc8f60924ff0160
Franck <franck.spadotto@yahoo.com>
2025-09-29 11:41:09 +0200

    Append text in a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""
+2-append_write.py
+Write in a file
+"""
+
+def append_write(filename="", text=""):
+    """
+    append_write - Append text to a file
+    @filename: Name of the file
+    @text: Text to add
+    Return: Number of char written
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)

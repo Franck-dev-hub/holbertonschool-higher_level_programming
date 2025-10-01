7737652b6cce0cc76d5b07a9452dc47703a564af
Franck <franck.spadotto@yahoo.com>
2025-10-01 16:51:10 +0200

    Refactor code logic

--- python-serialization/task_04_net.py
+++ python-serialization/task_04_net.py
@@ -9,58 +9,53 @@ import socket
 import json
 
 
-def start_server():
+def start_server(host="127.0.0.1", port=12345):
     """
     start_server - Set up server using socket library
+    @host: Default host address
+    @port: Default port
     Return: Void
     """
-    # --- Create socket ---
+    # Create socket
     # AF_INET = IPv4
     # SOCK_STREAM = TCP (reliable server)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # --- Link socket to an adress and a port ---
-    host = "127.0.0.1" # Local adress
-    port = 12345
-    server_socket.bind((host, port))
-
-    # --- Open socket to listen mode ---
-    server_socket.listen(1) # 1 = max waiting connexion
-    print("Server listening on {}:{}".format(host, port))
-
-    # --- Accept entering connexion
-    client_socket, client_adress = server_socket.accept()
-    print("Connected with {}".format(client_adress))
-
-    # --- Receive data ---
-    data = client_socket.recv(1024) # 1024 = max octets
-
-    # --- Convert bytes in dictionary ---
-    data_str = data.decode("utf-8") # Convert bytes in string
-    data_dict = json.loads(data_str) # Deserialiaze json string to dictionary
-
-    # --- Display dict ---
-    print("Dictionary : ", data_dict)
-
-    # --- Close connexion ---
-    client_socket.close()
-    server_socket.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        # Link socket to an adress and a port
+        server_socket.bind((host, port))
+        # Open socket to listen mode
+        server_socket.listen()
+        print("Server listening on {}:{}".format(host, port))
+        # Accept entering connexion
+        client_socket, client_adress = server_socket.accept()
+        with client_socket:
+            print("Connected with {}".format(client_adress))
+            # Receive data
+            data = client_socket.recv(1024) # 1024 = max octets
+            if data:
+                try:
+                    # Deserialize json to dictionary
+                    data_dict = json.loads(data.decode("utf-8"))
+                    # Display dict
+                    print("Dictionary : ", data_dict)
+                except json.JSONDecodeError:
+                    print("Failed to decode JSON data")
 
 
-def send_data(dictionary):
+def send_data(data_dict, host="127.0.0.1", port=12345):
     """
     send_data - Function that acts as the client
-    @dictionary: Dictionary send by the client
+    @data_dict: Dictionary send by the client
+    @host: Default host address
+    @port: Default port
     Return: Void
     """
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = "127.0.0.1"
-    port = 12345
-
-    client_socket.connect((host, port))
-
-    data_str = json.dumps(dictionary)
-
-    client_socket.send(data_str.encode("utf-8"))
-
-    client_socket.close()
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((host, port))
+            data_str = json.dumps(data_dict).encode("utf-8")
+            client_socket.sendall(data_str)
+            print("Data sent to the server successfully")
+    except ConnectionRefusedError:
+        print("Connection refused to the server")
+    except Exception as error:
+        print("An error occurred : {}".format(error))

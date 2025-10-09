6693158b8b7b4d78732f9e7d1c650e145986d6da
Franck <franck.spadotto@yahoo.com>
2025-10-09 15:32:50 +0200

    import json

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from flask import Flask, jsonify, request
+import json
 
 """
 task_04_flask.py
@@ -26,8 +27,7 @@ def get_usernames():
     get_usernames - Get usernames
     Return: list of users
     """
-    usernames = [username for username in users.keys()]
-    return (jsonify(usernames))
+    return jsonify(list(users.keys()))
 
 
 @app.route("/users/<username>")
@@ -36,12 +36,9 @@ def get_user(username):
     get_user - Get user datas
     Return: User data if user exist, else error 404
     """
-    if username in users.keys():
-        user = users[username]
-        return (jsonify(user))
-    else:
-        error = {"error": "User not found"}
-        return (jsonify(error), 404)
+    if username in users:
+        return jsonify(users[username])
+    return jsonify({"error": "User not found"}), 404
 
 
 @app.route("/add_user", methods=["POST"])
@@ -50,28 +47,20 @@ def add_user():
     add_user - Add user function
     Return: Message 201 if user added, else error 400
     """
-    if request.method == "POST":
-        content = json.loads(request.data)
+    content = json.loads(request.data)
 
-        if "username" in content.keys():
-            user = {
-                "username": content["username"],
-                "name": content["name"],
-                "age": content["age"],
-                "city": content["city"]
+    if "username" in content.keys():
+        return jsonify({"error": "Missing field"}), 400
+
+    user = {
+            "username": content["username"],
+            "name": content["name"],
+            "age": content["age"],
+            "city": content["city"]
             }
 
-            users[user["username"]] = user
-            message = "User added"
-
-            return (jsonify({
-                "message": message,
-                "user": user
-            }), 201)
-
-        else:
-            message = {"error": "Username is required"}
-            return (jsonify(message), 400)
+    users[user["username"]] = user
+    return jsonify({"message": "User added", "user": user}), 201
 
 
 @app.route("/status")

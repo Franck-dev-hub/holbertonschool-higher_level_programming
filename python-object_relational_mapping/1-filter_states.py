b3cf6d0c80b0f16a7de81d129d16beaa94f5a482
Franck <franck.spadotto@yahoo.com>
2025-10-26 11:32:24 +0100

    List states starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,48 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+"""
+List all states with names starting with N
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    main - Main function
+    Return: Void
+    """
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:4]
+    db = None
+
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database,
+                charset="utf8"
+                )
+
+        with db.cursor() as cur:
+            cur.execute("""SELECT * FROM states
+                        WHERE name LIKE BINARY 'N%'
+                        ORDER BY id ASC""")
+
+            for row in cur.fetchall():
+                print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL error: {}".format(e))
+
+    finally:
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    main()

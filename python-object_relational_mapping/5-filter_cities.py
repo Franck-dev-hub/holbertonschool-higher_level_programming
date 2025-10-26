280fb2af4d13b5832991344e3be940410d1f518a
Franck <franck.spadotto@yahoo.com>
2025-10-26 12:55:22 +0100

    Display cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,56 @@
 #!/usr/bin/python3
+"""
+List specific states
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    main - Main function
+    Return: Void
+    """
+    if len(sys.argv) != 5:
+        print("Usage: {} <user> <passwd> <db> <statename>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1:5]
+    db = None
+
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database,
+                charset="utf8"
+                )
+
+        with db.cursor() as cur:
+            sql = """
+                SELECT cities.name FROM cities
+                INNER JOIN states
+                ON states.id = cities.state_id
+                WHERE states.name = %s
+                ORDER BY cities.id ASC
+                """
+            cur.execute(sql, (state_name,))
+            rows = cur.fetchall()
+
+            if rows:
+                print(", ".join(row[0] for row in rows))
+            else:
+                print()
+
+    except MySQLdb.Error as e:
+        print("MySQL error: {}".format(e))
+
+    finally:
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    main()

0142c1b9d155dededbf92545d23a2be3a14eb52c
Franck <franck.spadotto@yahoo.com>
2025-10-26 11:55:54 +0100

    Same as task 2

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,51 @@
 #!/usr/bin/python3
+"""
+List all states with names match the argument
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    main - Main function
+    Return: Void
+    """
+    if len(sys.argv) != 5:
+        print("Usage: {} <user> <passwd> <db> <statename>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1:5]
+    db = None
+
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database,
+                charset="utf8"
+                )
+
+        with db.cursor() as cur:
+            sql = """
+                SELECT * FROM states
+                WHERE name = BINARY %s
+                ORDER BY id ASC
+                """
+            cur.execute(sql, (state_name,))
+
+            for row in cur.fetchall():
+                print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL error: {}".format(e))
+
+    finally:
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    main()

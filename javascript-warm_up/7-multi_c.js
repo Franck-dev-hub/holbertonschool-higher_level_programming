46c16ceaed6c2feab441384936c25717b09438a0
Franck <franck.spadotto@yahoo.com>
2025-11-05 14:54:07 +0100

    update indentation

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,9 +5,9 @@ const { argv } = require('node:process');
 if (argv.length > 2) {
   const num = Number(argv[2]);
   if (!isNaN(num)) {
-	  for (let i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
       console.log('My number: ', num);
-	  }
+    }
   } else {
     console.log('Missing number of occurrences');
   }

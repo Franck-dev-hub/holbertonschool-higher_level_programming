905c074e8dd429577f081bb452530d69190e560f
Franck <franck.spadotto@yahoo.com>
2025-11-05 13:58:05 +0100

    print a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv.length > 2) {
+  const num = Number(argv[2]);
+  if (!isNaN(num)) {
+	for (let i = 0; i < num; i++) {
+		for (let j = 0; j < num; j++) {
+			process.stdout.write('#');
+		}
+		console.log('#');
+	}
+  } else {
+    console.log('Missing size');
+  }
+}

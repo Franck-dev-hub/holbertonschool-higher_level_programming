30d894742b305c97d295a65499b4fd6eb0aebd7b
Franck <franck.spadotto@yahoo.com>
2025-11-05 12:00:30 +0100

    check if argv is a number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv.length > 2) {
+  const num = Number(argv[2]);
+  if (!isNaN(num)) {
+    console.log('My number: ', num);
+  } else {
+    console.log('Not a number');
+  }
+}

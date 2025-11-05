57eabd6b241597138115c8f3aa995b428560837b
Franck <franck.spadotto@yahoo.com>
2025-11-05 17:30:26 +0100

    print second highest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function second (argv) {
+  const nums = [];
+  for (let i = 2; i < argv.length; i++) {
+    nums.push(argv[i]);
+  }
+  return nums.sort().reverse();
+}
+
+if (argv.length === 2 || argv.length === 3) {
+  console.log(0);
+} else if (argv.length > 2) {
+  console.log(second(argv)[1]);
+}

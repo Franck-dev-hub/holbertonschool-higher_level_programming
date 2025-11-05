a8822965530bda30ac9c69b5da9618b905a576d2
Franck <franck.spadotto@yahoo.com>
2025-11-05 13:57:51 +0100

    print with a loop

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ if (argv.length > 2) {
   const num = Number(argv[2]);
   if (!isNaN(num)) {
 	  for (let i = 0; i < num; i++) {
-		console.log('My number: ', num);
+      console.log('My number: ', num);
 	  }
   } else {
     console.log('Missing number of occurrences');

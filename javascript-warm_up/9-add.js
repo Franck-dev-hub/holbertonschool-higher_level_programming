8135063bbec15665d11d43f25fdff4fea6c6f70f
Franck <franck.spadotto@yahoo.com>
2025-11-05 15:17:27 +0100

    add two numbers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function add (a, b) {
+  return a + b;
+}
+
+if (argv.length === 4) {
+  const num1 = Number(argv[2]);
+  const num2 = Number(argv[3]);
+
+  if (!isNaN(num1) && !isNaN(num2)) {
+    console.log(add(num1, num2));
+  } else {
+    console.log('NaN');
+  }
+}

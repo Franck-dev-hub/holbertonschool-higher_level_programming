91e91ca29605248c77161dc60d38c5e48253d09a
Franck <franck.spadotto@yahoo.com>
2025-11-06 09:11:30 +0100

    use repeat function

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,15 +3,10 @@
 const { argv } = require('node:process');
 const num = parseInt(argv[2]);
 
-if (argv.length > 2) {
-  if (isNaN(num)) {
-    console.log('Missing size');
-  } else {
-    for (let i = 0; i < num; i++) {
-      for (let j = 0; j < num; j++) {
-        process.stdout.write('X');
-      }
-      console.log('X');
-    }
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
   }
 }

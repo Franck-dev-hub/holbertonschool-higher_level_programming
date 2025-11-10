8857ef686f280a574baf303176f3b2269a6be9aa
Franck <franck.spadotto@yahoo.com>
2025-11-10 10:33:52 +0100

    add red class on click

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+function changeToRed () {
+  document.querySelector('header').classList.add('red');
+}
+
+document.getElementById('red_header').addEventListener('click', changeToRed);

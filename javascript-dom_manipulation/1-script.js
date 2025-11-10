38ddc63c7811de80a0d591fd71bf31a8111b927f
Franck <franck.spadotto@yahoo.com>
2025-11-10 10:30:10 +0100

    cahnge color on click

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+function changeToRed () {
+  document.querySelector('header').style.color = '#FF0000';
+}
+
+document.getElementById('red_header').addEventListener('click', changeToRed);

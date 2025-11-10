c8a4bd08fcb89a7f1afabfd66013c4b2e04ee9b5
Franck <franck.spadotto@yahoo.com>
2025-11-10 10:56:14 +0100

    add items

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+function addItems () {
+  const newItem = document.createElement('li');
+  newItem.textContent = 'Item';
+  document.querySelector('ul.my_list').appendChild(newItem);
+}
+
+document.getElementById('add_item').addEventListener('click', addItems);

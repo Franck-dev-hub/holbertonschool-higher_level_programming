84b8961f0b11560d48727098d18a1228275c6cca
Franck <franck.spadotto@yahoo.com>
2025-11-10 11:18:04 +0100

    fetch star wars lmovies from api

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.results.length; i++) {
+      const newItem = document.createElement('li');
+      newItem.innerText = data.results[i].title;
+      document.querySelector('ul').appendChild(newItem);
+    }
+  })

1572c2e68f236e8bf0eee86356048edb0f02313d
Franck <franck.spadotto@yahoo.com>
2025-11-10 10:48:16 +0100

    toggle colors

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+function toggleColor () {
+  const header = document.querySelector('header');
+  header.classList.toggle('red');
+  header.classList.toggle('green');
+}
+
+document.getElementById('toggle_header').addEventListener('click', toggleColor);
